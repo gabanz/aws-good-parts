65271fa590e1c51607ade8b339199f44ed258f24
Faiz Azhar <faiz@cloudflare.com>
2020-01-08 02:15:11 +0800

    Create infrastructure via CloudFormation

---
+++ deploy-infra.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+STACK_NAME=aws-good-parts
+REGION=ap-southeast-1
+CLI_PROFILE=aws-good-parts
+
+EC2_INSTANCE_TYPE=t2.micro
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+  aws cloudformation list-exports \
+    --profile aws-good-parts \
+    --query "Exports[?Name=='InstanceEndpoint'].Value"
+fi
+
