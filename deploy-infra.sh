61b91c9ddbf4723da390913c54aa1a7c277336ab
Faiz Azhar <faiz@cloudflare.com>
2020-01-10 19:15:13 +0800

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,6 +6,20 @@ CLI_PROFILE=aws-good-parts
 
 EC2_INSTANCE_TYPE=t2.micro
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \ ❶ --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ===========" aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
