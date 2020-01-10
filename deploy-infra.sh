0fb44d0d0bd64610e7813c165a45fe0f75eba7da
Faiz Azhar <faiz@cloudflare.com>
2020-01-11 01:19:01 +0800

    Add deployment pipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,11 +6,20 @@ CLI_PROFILE=aws-good-parts
 
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \ ❶ --query "Account" --output text`
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile aws-good-parts \
+ --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-good-parts-access-token)
+GH_OWNER=$(cat ~/.github/aws-good-parts-owner)
+GH_REPO=$(cat ~/.github/aws-good-parts-repo)
+GH_BRANCH=master
+
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ===========" aws cloudformation deploy \
+echo -e "\n\n=========== Deploying setup.yml ===========" 
+aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME-setup \
@@ -30,7 +39,12 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
